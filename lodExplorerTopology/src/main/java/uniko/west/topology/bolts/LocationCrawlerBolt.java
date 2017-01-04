7812e1a2638127f21883d286d012c2aa1ef78f4f
eszk <e.saizki@gmail.com>
2017-01-04 15:46:27 +0100

    updated debug info

--- lodExplorerTopology/src/main/java/uniko/west/topology/bolts/LocationCrawlerBolt.java
+++ lodExplorerTopology/src/main/java/uniko/west/topology/bolts/LocationCrawlerBolt.java
@@ -251,6 +251,7 @@ public class LocationCrawlerBolt extends BaseRichBolt {
 				locationDictionaryGet = (List<Object>) locationSet.get(j);}
 				Map<Object, Object> locationDictionary = new HashMap();
 				locationDictionary.put(locationDictionaryGet.get(0), locationDictionaryGet.get(1));
+				locationDictionary.put(locationDictionaryGet.get(0), locationDictionaryGet.get(1));
 				
 				if (locationDictionary.isEmpty()) geospatialContext.put("locationDictionary is empty", "true");
 				else {
