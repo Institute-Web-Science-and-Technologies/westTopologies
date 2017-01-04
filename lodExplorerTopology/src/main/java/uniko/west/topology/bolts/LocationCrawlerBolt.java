702970d4c03d0b9b9961565183ceeb1256b60708
eszk <e.saizki@gmail.com>
2017-01-04 21:16:26 +0100

    modified JSON parsing

--- lodExplorerTopology/src/main/java/uniko/west/topology/bolts/LocationCrawlerBolt.java
+++ lodExplorerTopology/src/main/java/uniko/west/topology/bolts/LocationCrawlerBolt.java
@@ -15,14 +15,11 @@ import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import org.json.JSONObject;
 
 import com.hp.hpl.jena.query.ParameterizedSparqlString;
 import com.hp.hpl.jena.query.Query;
@@ -254,20 +251,12 @@ public class LocationCrawlerBolt extends BaseRichBolt {
 				locationDictionaryGet = (List<Object>) locationSet.get(j);}
 				geospatialContext.put("debug - locationDictionaryGet containts " + locationDictionaryGet.size() + " item(s)", "true");
 				
-				Map<Object, Object> locationDictionary = new HashMap();
-				JSONObject jObject = new JSONObject(locationDictionaryGet);
-				
-				Iterator<?> keys = jObject.keys();
-
-		        while( keys.hasNext() ){
-		            String key = (String)keys.next();
-		            String value = jObject.getString(key); 
-		            locationDictionary.put(key, value);
-
-		        }
-				
-				//for (int j= 0; j < locationDictionaryGet.size(); j++){
-				//locationDictionary.put(locationDictionaryGet.get(j), 0);}
+				Map<Object, Object> locationDictionary = new HashMap<Object,Object>();
+				for (int j= 0; j < locationDictionaryGet.size(); j++){
+					//split by '='
+					String[] split = ((String) locationDictionaryGet.get(j)).split("=");
+					
+				locationDictionary.put(split[0], split[1]);}
 				
 				if (locationDictionary.isEmpty()) geospatialContext.put("locationDictionary is empty", "true");
 				else {
